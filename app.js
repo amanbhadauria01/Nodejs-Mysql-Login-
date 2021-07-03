e3788786f4fe81a25309cb8478aee630b8e3b219
amanbhadauria01 <amanbhadauria01@gmail.com>
2021-07-03 15:47:31 +0530

    updates

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 dotenv.config({path : './.env'});
+const PORT = process.env.PORT || 3000; 
 
 const db = mysql.createConnection({
     host     : process.env.DATABASE_HOST,
@@ -29,6 +30,6 @@ app.set('view engine','hbs');
 
 app.use('/',require('./routes/pages.js'));
 app.use('/auth',require('./routes/auth.js'));
-app.listen(3000,()=>{
-    console.log("Server started at 3000");
+app.listen(PORT,()=>{
+    console.log(`Listening on Port ${PORT}`);
 })
